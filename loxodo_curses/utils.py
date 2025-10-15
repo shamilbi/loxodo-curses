8f05128d91f3ceb0a0075fcf66573721960edb7d
shamilbi <shamilbi@gmail.com>
2025-10-15 19:33:34 +0200

    fix: input_file(): +os.path.expanduser() (~/filename)

--- loxodo_curses/utils.py
+++ loxodo_curses/utils.py
@@ -35,6 +35,7 @@ def input_file(prompt: str):
         s = s.strip()
         if not s:
             continue
+        s = os.path.expanduser(s)  # ~/filename
         if os.path.isfile(s) or not os.path.exists(s):
             return s
         print(f'{s} is not a file')
